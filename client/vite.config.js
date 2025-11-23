e7a10cf1d949da4099a39ebeb0bcc6b5f03ce03f
41371125h-chinrouzhen <41371125h@gapps.ntnu.edu.tw>
2025-11-23 19:54:55 +0800

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import mkcert from 'vite-plugin-mkcert' 
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: { 
-    https: true 
-  }, 
-  plugins: [
-    react(), 
-    mkcert() 
-  ],
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, 
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+
+          if (id.includes('node_modules')) {
+            return 'vendor'; 
+          }
+          
+          // if (id.includes('node_modules')) {
+          //     if (id.includes('three')) {
+          //         return 'three_lib';
+          //     }
+          //     if (id.includes('firebase')) {
+          //         return 'firebase_lib';
+          //     }
+          //     return 'vendor'; 
+          // }
+        }
+      }
+    }
+  }
 })
