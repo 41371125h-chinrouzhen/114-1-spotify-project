e938074fd7cd512f5010c3192fc58f26d1561387
41371125h-chinrouzhen <41371125h@gapps.ntnu.edu.tw>
2025-11-23 20:22:53 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,13 +15,13 @@ import PlaylistUI from './components/PlaylistUI.jsx';
 import BackButton from './components/BackButton.jsx';
 
 // [修改 2] 請在此填入您的 Firebase 設定
-const LOCAL_FIREBASE_CONFIG = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.firebasestorage.app",
-    messagingSenderId: "...",
-    appId: "..."
+const firebaseConfig = {
+    apiKey: "AIzaSyBZmXt6xfFFZ29eDGG-7tHzT7MtJsc7eQE",
+    authDomain: "spoti-24a7e.firebaseapp.com",
+    projectId: "spoti-24a7e",
+    storageBucket: "spoti-24a7e.firebasestorage.app",
+    messagingSenderId: "21554059222",
+    appId: "1:21554059222:web:f5a6c2b1561e7c6456b677"
 };
 
 function App() {
